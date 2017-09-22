362e625195c4aa24dc654f017a64c45754675977
rfdenk <rfdenk@gmail.com>
2017-09-22 10:59:53 -0700

    tweaks

--- spread.py
+++ spread.py
@@ -1,32 +1,43 @@
 
-kwargs = {'a': 12, 'b':'abc'}
+kw = {'a': 12, 'b': 'abc'}
 
-print(kwargs)
+print(kw)
 
-def f2(a=0,b=''):
+
+def f2(*args, a=0, b='_'):
+    print('f2a', args)
     print('f2', a, b)
 
-def f(**kwargs):
-    for n in kwargs:
-        print('f', n, kwargs[n])
-    f2(**kwargs)
 
-f(**kwargs)
+def f(*args, **kwargs):
+    for a in args:
+        print('fa', a)
+    for n in kwargs:
+        print('fkw', n, kwargs[n])
+    f2(*args, **kwargs)
+
+
+f(kw)
+f(**kw)
 
 lst = (1, 2, 3)
 print("list", lst)
+
 
 def fl(*args):
     for n in args:
         print('fl', n)
 
+
 fl(lst)
 fl(1, 2, 3)
+
 
 def fl2(*args):
     print('fl2')
     fl(args)
     fl(*args)
+
 
 fl2(lst)
 fl2(1, 2, 3)
