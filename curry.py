0bda7c8a12404c2842bd22d2ef327b7731c5f391
rfdenk <rfdenk@gmail.com>
2017-09-25 09:22:17 -0700

    changed a parameter name, just for fun

--- curry.py
+++ curry.py
@@ -4,19 +4,18 @@ def curry(f, *fixed_args, **fixed_kwargs):
     f_kwargs = dict(**fixed_kwargs)
 
     def __curry(*args, **kwargs):
-        new_kwargs = f_kwargs;
+        new_kwargs = f_kwargs
         new_kwargs.update(kwargs)
         new_args = f_args + args
         return f(*new_args, **new_kwargs)
     return __curry
 
 
-def func(a,b,c, d=1):
-    return (a + b + c) * d
+def func(a, b, c, multiply_by=1):
+    return (a + b + c) * multiply_by
 
 
-curried_func = curry(func, 1, 2, d=3)
+curried_func = curry(func, 1, 2, multiply_by=3)
 
 print(curried_func(3))
-print(curried_func(3, d=2))
-
+print(curried_func(3, multiply_by=2))
