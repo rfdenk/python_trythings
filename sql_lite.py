76dfa8e86c40c9864a10a2710ae4db9e143cc2e6
rfdenk <rfdenk@gmail.com>
2017-09-26 11:27:05 -0700

    more examples of various SQL functions.

--- sql_lite.py
+++ sql_lite.py
@@ -30,18 +30,19 @@ except sqlite3.OperationalError:
 c.executemany("INSERT INTO cities VALUES (?,?)", cities)
 
 try:
-    c.execute('CREATE TABLE characters (name text, city int, moved int)')
-except sqlite3.OperationalError:
-    # already exists!
-    pass
+    c.execute('CREATE TABLE characters (id integer primary key autoincrement, name text, city integer, age integer, moved integer, movedon DATE)')
+#except sqlite3.OperationalError as e:
+    #print(e)
+except Exception as e:
+    print(e)
 
-owners = [
-        ('Bilbo', 1, 1),
-        ('Innkeeper', 2, 0),
-        ('Aragorn', 2, 3),
+characters = [
+        ('Bilbo', 1, 65, 1),
+        ('Innkeeper', 2, 65, 0),
+        ('Aragorn', 2, 137, 3),
     ]
 
-c.executemany('INSERT INTO characters VALUES (?,?,?)', owners)
+c.executemany('INSERT INTO characters (name, city, age, moved) VALUES (?, ?, ?, ?)', characters)
 
 conn.commit()
 
@@ -59,6 +60,61 @@ print(c.fetchall())
 
 print()
 print()
+
+c.execute('SELECT city FROM characters')
+print("NON-DISTINCT RESIDENCE =", c.fetchall())
+c.execute('SELECT DISTINCT city FROM characters')
+print("DISTINCT RESIDENCE =", c.fetchall())
+c.execute('SELECT min(age), max(age) FROM characters')
+print('AGE RANGE =', c.fetchall())
+
+c.execute('SELECT COUNT(DISTINCT age) FROM characters')
+print('AGE CT =', c.description, c.fetchall())
+c.execute('SELECT COUNT(DISTINCT age) AS num_ages FROM characters')
+print('AGE CT =', c.description, c.fetchall())
+c.execute('SELECT count(*) AS DistinctAges FROM (SELECT DISTINCT age FROM characters)')
+print('AGE COUNT =', c.description, c.fetchall())
+c.execute('UPDATE characters SET age=64 WHERE name="Innkeeper"')
+c.execute('SELECT count(*) AS DistinctAges FROM (SELECT DISTINCT age FROM characters)')
+print('AGE COUNT =', c.description, c.fetchall())
+
+c.execute('SELECT avg(age) AS avg_age FROM characters')
+print('AVG AGE =', c.fetchall())
+
+
+c.execute('SELECT * FROM characters WHERE name LIKE "%n%"')
+print("Names with n =", c.fetchall())
+
+c.execute('SELECT * FROM characters WHERE name NOT LIKE "%n%"')
+print("Names without n =", c.fetchall())
+
+c.execute('SELECT * FROM characters WHERE name LIKE "%[rn]%"')
+print("Names with r or n =", c.fetchall())          # doesn't work in sqlite?
+
+c.execute('SELECT * FROM characters WHERE age IN (65, 137)')
+print("with age in 65, 137 =", c.fetchall())
+
+c.execute('SELECT * FROM characters WHERE city IN (SELECT id FROM cities WHERE name="Bree")')
+print("lives in Bree =", c.fetchall())
+
+
+c.execute('SELECT name, age + city AS agecity FROM characters')
+print(c.fetchall())
+
+
+c.execute('SELECT name FROM characters UNION SELECT id FROM cities')
+print("UNION name,id =", c.description, c.fetchall())
+
+
+c.execute('SELECT COUNT(characters.name), cities.name FROM characters JOIN cities ON characters.city = cities.id')
+print("in each city NO GROUP =", c.fetchall())
+
+
+c.execute('SELECT COUNT(characters.name), cities.name FROM characters JOIN cities ON characters.city = cities.id GROUP BY cities.name')
+print("in each city =", c.fetchall())
+print()
+print()
+
 
 #
 # JOIN
@@ -160,17 +216,27 @@ create_addr_change_trigger = (
         )
 c.execute(create_addr_change_trigger)
 
-c.execute('UPDATE characters SET city=3 WHERE name="Bilbo"')
-c.execute('SELECT moved FROM characters WHERE name="Bilbo"')
-print(c.fetchall()[0][0])  # should be 2
+create_addr_change_trigger2 = (
+        'CREATE TRIGGER address_change2 '
+        'BEFORE UPDATE OF city ON characters '
+        'BEGIN '
+        'UPDATE characters SET movedon=date("now") WHERE name=NEW.name AND OLD.city <> NEW.city;'
+        'END;'
+        )
+c.execute(create_addr_change_trigger2)
 
 c.execute('UPDATE characters SET city=3 WHERE name="Bilbo"')
-c.execute('SELECT moved AS moved FROM characters WHERE name="Bilbo"')
-print(c.fetchone()[0])     # should still be 2, character's new city is same as old city
+c.execute('SELECT moved, movedon FROM characters WHERE name="Bilbo"')
+print(c.fetchall())  # should be 2, today
+
+
+c.execute('UPDATE characters SET city=3 WHERE name="Bilbo"')
+c.execute('SELECT moved, movedon FROM characters WHERE name="Bilbo"')
+print(c.fetchone())     # should still be 2, today, character's new city is same as old city
 
 c.execute('UPDATE characters SET city=1 WHERE name="Bilbo"')
-c.execute('SELECT moved FROM characters WHERE name="Bilbo"')
-print(c.fetchall()[0][0])  # should now be 3; character moved from city 3 to city 1
+c.execute('SELECT moved, movedon FROM characters WHERE name="Bilbo"')
+print(c.fetchall())  # should now be 3, today; character moved from city 3 to city 1
 
 
 #
