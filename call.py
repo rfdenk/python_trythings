d816c44ee7018e94ff746d7177fdd23a3ebacc91
rfdenk <rfdenk@gmail.com>
2017-09-26 13:47:37 -0700

    stuff

--- call.py
+++ call.py
@@ -21,6 +21,23 @@ def func_b(*args, **kwargs):
 # direct decoration...
 func_a = DoSomething(func_a)
 
+print(dir(func_a))
+print(dir(DoSomething))
+print(callable(func_a))
+print(callable(func_b))
 
 func_a('a', 'b', flush=True, end='[EOL]\r\n', sep='.')
-func_b('x', 'y', 'z', end='</msg>', sep='<>')
+func_b('x', 'y', 'z', end='</msg>\r\n', sep='<>')
+
+
+
+class NoCall:
+    def __init__(self, v):
+        self.v = v
+
+
+nc = NoCall(3)
+print(dir(nc))
+print(callable(nc))
+if callable(nc):
+    nc()
